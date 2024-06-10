d1574e374b3fbcbe2aec72f0c08d5665a9217e6f
Ace <aevancho@me.com>
2024-06-10 10:10:41 -0500

    Work on Goals frontend

--- client/src/components/ContainerComponents/AllEntries.tsx
+++ client/src/components/ContainerComponents/AllEntries.tsx
@@ -22,6 +22,9 @@ import {
 } from "@radix-ui/react-accordion";
 import { Progress } from "@/components/ui/progress"
 import { Badge } from "../ui/badge";
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
+import { Separator } from "@/components/ui/separator"
 
 interface Entry {
     _id: string;
@@ -91,6 +94,12 @@ const AllEntries: React.FC = () => {
     }));
   };
 
+  interface SentimentCardProps {
+    entry: {
+      sentiment: number;
+    };
+  }
+
   const SentimentCard: React.FC<SentimentCardProps> = ({ entry }) => {
     const sentimentPercentage = ((entry.sentiment + 1) / 2) * 100;
     const sentimentLabel = entry.sentiment > 0.2 ? 'Positive' : entry.sentiment < -0.2 ? 'Negative' : 'Neutral';
@@ -157,15 +166,26 @@ const AllEntries: React.FC = () => {
                 </AccordionTrigger>
                 <AccordionContent asChild>
                   <CardContent className="flex flex-col">
+                    <p className="my-1 leading-7">{entry.text}...</p>
+                    <Separator className="my-4" />
                     <div className="flex items-center justify-evenly">
                       <p className="w-3/4">{entry.tags.map((t) => <Badge className="mx-1 leading-7" variant="outline">{t}</Badge>)}</p>
                       <SentimentCard entry={entry} />                    
                     </div>
-                    <h4 className="leading-7 scroll-m-20 text-2xl font-semibold tracking-tight">Entry</h4>
-                    <p className="my-1 leading-7">{entry.text}...</p>
                     <h4 className="leading-7 scroll-m-20 text-2xl font-semibold tracking-tight">Analysis</h4>
                     <p className="my-1 leading-7">Analysis {entry.analysis}...</p>
-                    <p>Goals:{entry.goals.map((g) => <p className="my-1 leading-7">{g}</p>)}</p>
+                    <div className="flex flex-col">
+                      <h4 className="leading-7 scroll-m-20 text-2xl font-semibold tracking-tight">Goals</h4>
+                      {entry.goals.map((g, index) => {
+                      const goalId = `${entry._id}-goal-${index}`
+                      return (
+                      // <p className="my-1 leading-7">{g}</p>
+                      <div className="flex items-center justify-between space-x-2 my-1">
+                        <Label htmlFor={goalId}>{g}</Label>
+                        <Switch id={goalId} />
+                      </div>
+                    )
+                    })}</div>
                     <p>Encouragements:{entry.encouragements.map((e) => <p className="my-1 leading-7">{e}</p>)}</p>
                     <p>Questions:{entry.questions.map((q) => <p className="my-1 leading-7">{q}</p>)}</p>
                   </CardContent>
