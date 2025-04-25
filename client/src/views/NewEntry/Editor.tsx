c08ffc76e14785439c58ea06207fc56413349b0e
acevancho <aevancho@me.com>
2025-04-25 12:45:10 -0500

    Big changes to New Entry Toolbar

--- client/src/views/NewEntry/Editor.tsx
+++ client/src/views/NewEntry/Editor.tsx
@@ -1,27 +1,39 @@
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
-import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
+import { TabIndentationPlugin } from "@lexical/react/LexicalTabIndentationPlugin";
+import ToolbarPlugin from "./ToolbarPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
-import ToolbarPlugin from "./ToolbarPlugin";
 import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
-import { HeadingNode, QuoteNode } from '@lexical/rich-text';
-import { ListNode, ListItemNode } from '@lexical/list';
-import { CodeNode } from '@lexical/code';
-import { LinkNode } from '@lexical/link';
-import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
-import { $getRoot } from 'lexical';
-
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { ListNode, ListItemNode } from "@lexical/list";
+import { CodeNode } from "@lexical/code";
+import { LinkNode } from "@lexical/link";
+import { HorizontalRuleNode } from "@lexical/react/LexicalHorizontalRuleNode";
+import { $getRoot, ParagraphNode } from "lexical";
 
 const editorConfig = {
   namespace: "JournalEditor",
   theme: {
     text: {
-        bold: 'font-bold',
-        italic: 'italic',
-        underline: 'underline',
+      bold: "font-bold",
+      italic: "italic",
+      underline: "underline",
+      strikethrough: "strikethrough",
+    },
+    list: {
+        nested: {
+          listitem: 'editor-nested-listitem',
+        },
+        ol: 'editor-list-ol',
+        ul: 'editor-list-ul',
+        listitem: 'editor-listItem',
+        listitemChecked: 'editor-listItemChecked',
+        listitemUnchecked: 'editor-listItemUnchecked',
       },
   },
   onError(error: Error) {
@@ -35,36 +47,35 @@ const editorConfig = {
     CodeNode,
     LinkNode,
     HorizontalRuleNode,
+    ParagraphNode,
   ],
 };
 
 interface EditorProps {
-    setText: (v: string) => void;
-    handleGetPrompts: () => void;
-    promptsLoading: boolean;
-    prompts: string[];
-    setPrompt: (v: string) => void;
-    isEditing: boolean;
+  setText: (v: string) => void;
+  handleGetPrompts: () => void;
+  promptsLoading: boolean;
+  prompts: string[];
+  setPrompt: (v: string) => void;
+  isEditing: boolean;
 }
 
 export default function Editor(props: EditorProps) {
   return (
-    <LexicalComposer initialConfig={editorConfig} >
-      <div className="mb-4 border rounded flex flex-col h-full">
-      
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className="mb-4 flex flex-col h-full">
         <RichTextPlugin
           contentEditable={
-              <ContentEditable 
-              className="p-4 outline-none h-full"
-              />
-            }
-        //   placeholder={
-        //       <div className="p-4 text-md text-muted-foreground">
-        //     Let it all out...</div>
-        // }
+            <ContentEditable className="p-4 outline-none h-full" />
+          }
+          placeholder={
+            <div className="p-4 text-sm text-muted-foreground absolute ">
+              Let it all out...
+            </div>
+          }
           ErrorBoundary={LexicalErrorBoundary}
         />
-                <OnChangePlugin
+        <OnChangePlugin
           onChange={(editorState) => {
             editorState.read(() => {
               const text = $getRoot().getTextContent();
@@ -73,15 +84,17 @@ export default function Editor(props: EditorProps) {
           }}
         />
         <MarkdownShortcutPlugin />
-        <ToolbarPlugin 
-            handleGetPrompts={props.handleGetPrompts}
-            promptsLoading={props.promptsLoading}
-            prompts={props.prompts}
-            setPrompt={props.setPrompt}
-            isEditing={props.isEditing}/>
+        <ToolbarPlugin
+          handleGetPrompts={props.handleGetPrompts}
+          promptsLoading={props.promptsLoading}
+          prompts={props.prompts}
+          setPrompt={props.setPrompt}
+          isEditing={props.isEditing}
+        />
         <HistoryPlugin />
         <AutoFocusPlugin />
-        
+        <TabIndentationPlugin />
+        <ListPlugin />
       </div>
     </LexicalComposer>
   );
--- client/src/views/NewEntry/ToolbarPlugin.tsx
+++ client/src/views/NewEntry/ToolbarPlugin.tsx
@@ -1,30 +1,78 @@
 import {
   FORMAT_TEXT_COMMAND,
+  FORMAT_ELEMENT_COMMAND,
   $getSelection,
   $isRangeSelection,
   TextFormatType,
+  ElementFormatType,
 } from "lexical";
+import { $getNearestNodeOfType, $findMatchingParent } from "@lexical/utils";
+import {
+  INSERT_UNORDERED_LIST_COMMAND,
+  INSERT_ORDERED_LIST_COMMAND,
+  REMOVE_LIST_COMMAND,
+  ListNode,
+  ListNodeTagType,
+  $isListItemNode,
+  $isListNode,
+} from "@lexical/list";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { useCallback, useEffect, useState } from "react";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { Bold, Italic, Underline } from "lucide-react";
-import { CornerDownLeft, Mic } from "lucide-react";
+import {
+  Bold,
+  Italic,
+  Underline,
+  CornerDownLeft,
+  Mic,
+  LucideStrikethrough,
+  AlignRightIcon,
+  AlignLeftIcon,
+  AlignCenter,
+  List,
+  ListOrdered,
+  EllipsisVertical,
+  Menu,
+  Strikethrough,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import GeneratePromptsBtn from "./GeneratePromptsBtn";
+import { Separator } from "@radix-ui/react-separator";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface NewEntryFormBtnBarProps {
-    handleGetPrompts: () => void;
-    promptsLoading: boolean;
-    prompts: string[];
-    setPrompt: (v: string) => void;
-    isEditing: boolean;
-  }
+  handleGetPrompts: () => void;
+  promptsLoading: boolean;
+  prompts: string[];
+  setPrompt: (v: string) => void;
+  isEditing: boolean;
+}
 
 export default function ToolbarPlugin(props: NewEntryFormBtnBarProps) {
   const [editor] = useLexicalComposerContext();
   const [activeFormats, setActiveFormats] = useState<string[]>([]);
+  const [activeAlignment, setActiveAlignment] = useState<string>("");
+  const [activeListType, setActiveListType] = useState<"ul" | "ol" | "">("");
 
   const updateToolbar = useCallback(() => {
     const selection = $getSelection();
@@ -33,7 +81,20 @@ export default function ToolbarPlugin(props: NewEntryFormBtnBarProps) {
       if (selection.hasFormat("bold")) formats.push("bold");
       if (selection.hasFormat("italic")) formats.push("italic");
       if (selection.hasFormat("underline")) formats.push("underline");
+      if (selection.hasFormat("strikethrough")) formats.push("strikethrough");
       setActiveFormats(formats);
+
+      const anchorNode = selection.anchor.getNode();
+      const element = anchorNode.getTopLevelElementOrThrow();
+      const format = element.getFormatType();
+      setActiveAlignment(format);
+
+      const parentList = $findMatchingParent(anchorNode, $isListNode);
+      if (parentList) {
+        setActiveListType(parentList?.__listType === "bullet" ? "ul" : "ol");
+      } else {
+        setActiveListType("");
+      }
     }
   }, []);
 
@@ -43,55 +104,195 @@ export default function ToolbarPlugin(props: NewEntryFormBtnBarProps) {
     });
   }, [editor, updateToolbar]);
 
-  const toggleFormat = (format: TextFormatType) => {
+  const toggleTextFormat = (format: TextFormatType) => {
     editor.dispatchCommand(FORMAT_TEXT_COMMAND, format);
   };
 
+  const toggleElementFormat = (format: ElementFormatType) => {
+    editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, format);
+  };
+
+  const toggleListFormat = (format: ListNodeTagType) => {
+    if (format === activeListType) {
+      return editor.dispatchCommand(REMOVE_LIST_COMMAND, undefined);
+    }
+    if (format === "ul") {
+      editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined);
+    } else if (format === "ol") {
+      editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND, undefined);
+    } else {
+      editor.dispatchCommand(REMOVE_LIST_COMMAND, undefined);
+    }
+  };
+
   return (
-    <div className="flex items-center p-3 pt-0 justify-between">
-    <div className="flex items-center gap-2">
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button type="button" variant="ghost" size="icon">
-              <Mic className="size-4" />
-              <span className="sr-only">Use Microphone</span>
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="top">Use Microphone</TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <GeneratePromptsBtn
-        handleGetPrompts={props.handleGetPrompts}
-        promptsLoading={props.promptsLoading}
-        prompts={props.prompts}
-        setPrompt={props.setPrompt}
-      />
-    </div>
-    <ToggleGroup
-        type="multiple"
-        value={activeFormats}
-        onValueChange={(newVals) => {
-            const diff = newVals.find(v => !activeFormats.includes(v)) ||
-                        activeFormats.find(v => !newVals.includes(v));
-            if (diff) toggleFormat(diff as TextFormatType);
-        }}
-        className="flex gap-2 px-3 py-2"
+    <div className="flex items-center p-3 pt-0 justify-between flex-wrap gap-2">
+      {/* Desktop controls */}
+      <div className="hidden md:flex items-center gap-2">
+        <ToggleGroup
+          type="single"
+          value={activeAlignment}
+          onValueChange={(val) =>
+            val && toggleElementFormat(val as ElementFormatType)
+          }
         >
-        <ToggleGroupItem value="bold" aria-label="Bold">
-            <Bold className="h-4 w-4" />
-        </ToggleGroupItem>
-        <ToggleGroupItem value="italic" aria-label="Italic">
-            <Italic className="h-4 w-4" />
-        </ToggleGroupItem>
-        <ToggleGroupItem value="underline" aria-label="Underline">
-            <Underline className="h-4 w-4" />
-        </ToggleGroupItem>
+          <ToggleGroupItem value="left">
+            <AlignLeftIcon className="h-4 w-4" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="center">
+            <AlignCenter className="h-4 w-4" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="right">
+            <AlignRightIcon className="h-4 w-4" />
+          </ToggleGroupItem>
         </ToggleGroup>
-    <Button type="submit" size="sm" className="">
-      {props.isEditing ? "Update" : "Submit"}
-      <CornerDownLeft className="size-3.5" />
-    </Button>
-  </div>
+        <Separator orientation="vertical" className="h-5 w-px bg-border mx-2" />
+        <ToggleGroup
+          type="multiple"
+          value={activeFormats}
+          onValueChange={(newVals) => {
+            const diff =
+              newVals.find((v) => !activeFormats.includes(v)) ||
+              activeFormats.find((v) => !newVals.includes(v));
+            if (diff) toggleTextFormat(diff as TextFormatType);
+          }}
+        >
+          <ToggleGroupItem value="bold">
+            <Bold className="h-4 w-4" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="italic">
+            <Italic className="h-4 w-4" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="underline">
+            <Underline className="h-4 w-4" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="strikethrough">
+            <LucideStrikethrough className="h-4 w-4" />
+          </ToggleGroupItem>
+        </ToggleGroup>
+        <Separator orientation="vertical" className="h-5 w-px bg-border mx-2" />
+        <ToggleGroup
+          type="single"
+          value={activeListType}
+          onValueChange={(val) => toggleListFormat(val as ListNodeTagType)}
+        >
+          <ToggleGroupItem value="ul">
+            <List className="h-4 w-4" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="ol">
+            <ListOrdered className="h-4 w-4" />
+          </ToggleGroupItem>
+        </ToggleGroup>
+      </div>
+
+      {/* Ellipsis menu for mobile */}
+      <div className="block md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <Bold className="h-4 w-4" />
+              <Italic className="h-4 w-4" />
+              <Underline className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuGroup>
+              <DropdownMenuItem onClick={() => toggleTextFormat("bold")}>
+                Bold
+                <DropdownMenuShortcut>
+                  <Bold className="h-4 w-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => toggleTextFormat("italic")}>
+                Italics
+                <DropdownMenuShortcut>
+                  <Italic className="h-4 w-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => toggleTextFormat("underline")}>
+                Underline
+                <DropdownMenuShortcut>
+                  <Underline className="h-4 w-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => toggleTextFormat("strikethrough")}
+              >
+                Strikethrough
+                <DropdownMenuShortcut>
+                  <Strikethrough className="h-4 w-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem onClick={() => toggleElementFormat("left")}>
+                Align Left
+                <DropdownMenuShortcut>
+                  <AlignLeftIcon className="h-4 w-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => toggleElementFormat("center")}>
+                Align Center
+                <DropdownMenuShortcut>
+                  <AlignCenter className="h-4 w-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => toggleElementFormat("right")}>
+                Align Right
+                <DropdownMenuShortcut>
+                  <AlignRightIcon className="h-4 w-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem onClick={() => toggleListFormat("ol")}>
+                Ordered List
+                <DropdownMenuShortcut>
+                  <List className="h-4 w-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => toggleListFormat("ul")}>
+                Unordered List
+                <DropdownMenuShortcut>
+                  <ListOrdered className="h-4 w-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      <Separator orientation="vertical" className="h-5 w-px bg-border mx-2" />
+
+      {/* Prompt Generation & Mic */}
+      <div className="flex items-center gap-2">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button type="button" variant="ghost" size="icon">
+                <Mic className="size-4" />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="top">Use Microphone</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <GeneratePromptsBtn
+          handleGetPrompts={props.handleGetPrompts}
+          promptsLoading={props.promptsLoading}
+          prompts={props.prompts}
+          setPrompt={props.setPrompt}
+        />
+      </div>
+
+      <Separator orientation="vertical" className="h-5 w-px bg-border mx-2" />
+
+      {/* Submit Button */}
+      <Button type="submit" size="sm">
+        {props.isEditing ? "Update" : "Submit"}
+        <CornerDownLeft className="size-3.5" />
+      </Button>
+    </div>
   );
 }
